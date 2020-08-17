7f6802bc3db7b7db304f97fd7e776f162f595e8c
Piyush Barve <barvepiyush1000@gmail.com>
2020-08-17 21:06:01 +0530

    Code updated

--- Anime.js
+++ Anime.js
@@ -14,5 +14,9 @@ temp=action;}
 else if(genre=='Fighting'){temp=fighting;}
 else if(genre=='Movies'){temp=Movies;}
 else if(genre=='Thriller'){temp=thriller;}
-document.getElementById("demo").innerHTML= 'Top ' +genre+' Animes'+'<br>'+"1." + temp[0] + "<br>" + "2." + temp[1]+ '<br>' + "3."+ temp[2]+ "<br>" + "4." + temp[3]+ "<br>" + "5." + temp[4]  ;
+var text='';
+for(value of temp){
+    text += value + '<br>';
+}
+document.getElementById("demo").innerHTML= 'Top ' +genre+' Animes' +'<br>'+ text;
 }
